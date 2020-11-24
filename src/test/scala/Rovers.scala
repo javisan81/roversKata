031c91b47799649ed8b2d5e0a0cef2fbe648492b
Javier Lopez <javisan81@hotmail.com>
2020-11-24 07:41:41 +0100

    first test created

---
+++ src/test/scala/Rovers.scala
@@ -0,0 +1,11 @@
+
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+
+class Rovers extends AnyFunSpec{
+  describe("one test") {
+    it("should be true") {
+      true shouldBe true
+    }
+  }
+}
