0671b43625cca128e09f80a939acde42bca261b3
Javier Lopez <javisan81@hotmail.com>
2020-11-24 07:55:35 +0100

    Rovers can be created facing the north

--- src/test/scala/Rovers.scala
+++ src/test/scala/Rovers.scala
@@ -3,9 +3,19 @@ import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 
 class Rovers extends AnyFunSpec{
-  describe("one test") {
-    it("should be true") {
-      true shouldBe true
+  describe("Rovers") {
+    it("should be centered in 0,0 facing the north"){
+      val rover = Rover(Position(0,0), North)
+      rover.position shouldEqual Position(0,0)
+      rover.direction shouldEqual North
     }
   }
+}
+
+case class Position(x: Int, y: Int)
+
+abstract sealed case class Direction()
+object North extends Direction
+
+case class Rover(position: Position, direction: Direction) {
 }
