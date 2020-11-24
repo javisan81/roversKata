2fe7ee168b201c6ad0bfb445101a8f8c2e4b0804
Javier Lopez <javisan81@hotmail.com>
2020-11-24 07:57:17 +0100

    Add rest of directions (south, east, west)

--- src/test/scala/Rovers.scala
+++ src/test/scala/Rovers.scala
@@ -9,6 +9,21 @@ class Rovers extends AnyFunSpec{
       rover.position shouldEqual Position(0,0)
       rover.direction shouldEqual North
     }
+    it("should be centered in 1,1 facing the south"){
+      val rover = Rover(Position(1,1), South)
+      rover.position shouldEqual Position(1,1)
+      rover.direction shouldEqual South
+    }
+    it("should be centered in 1,1 facing the east"){
+      val rover = Rover(Position(1,1), East)
+      rover.position shouldEqual Position(1,1)
+      rover.direction shouldEqual East
+    }
+    it("should be centered in 1,1 facing the west"){
+      val rover = Rover(Position(1,1), West)
+      rover.position shouldEqual Position(1,1)
+      rover.direction shouldEqual West
+    }
   }
 }
 
@@ -16,6 +31,9 @@ case class Position(x: Int, y: Int)
 
 abstract sealed case class Direction()
 object North extends Direction
+object South extends Direction
+object East extends Direction
+object West extends Direction
 
 case class Rover(position: Position, direction: Direction) {
 }
