f52a8826e33efc751d1d70e71c66e3615dccdeec
Javier Lopez <javisan81@hotmail.com>
2020-11-26 11:34:57 +0100

    refactor moving duplicity

--- src/main/scala/com/rovers/Movement.scala
+++ src/main/scala/com/rovers/Movement.scala
@@ -1,5 +1,12 @@
 package com.rovers
 
-sealed trait Movement
-object Forward extends Movement
-object Backward extends Movement
+sealed class Movement(vectorByDirection:Map[Direction, (Int,Int)]){
+  def move(rover: Rover) = {
+    Rover(traverse(rover.position, vectorByDirection.getOrElse(rover.direction, (0,0))), rover.direction)
+  }
+  private def traverse(point: Position, vector: (Int, Int)) = {
+    Position(point.x + vector._1, point.y + vector._2)
+  }
+}
+object Forward extends Movement(Map(North -> (0,1), South -> (0,-1), East -> (1,0), West -> (-1, 0)))
+object Backward extends Movement(Map(North -> (0,-1), South -> (0,1), East -> (-1,0), West -> (1, 0)))
