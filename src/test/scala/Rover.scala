1f5bd0c3156bb060fa5dcbcd79e16ad098d33b70
Javier Lopez <javisan81@hotmail.com>
2021-02-23 09:20:04 +0100

    rover can move forward in any position facing north

--- src/test/scala/Rover.scala
+++ src/test/scala/Rover.scala
@@ -2,12 +2,14 @@ import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 
 abstract case class Direction()
+
 object North extends Direction
+
 case class Rover(x: Int, y: Int, direction: Direction) {
   def move(commands: Array[String]): Rover = {
     val firstCommand = commands(0)
     firstCommand match {
-      case "f" => Rover(0,1, North)
+      case "f" => Rover(x, y + 1, North)
       case "b" => Rover(0, -1, North)
     }
   }
@@ -15,20 +17,27 @@ case class Rover(x: Int, y: Int, direction: Direction) {
 
 class RoverTest extends AnyFunSpec {
   describe("Rover") {
-    it(s"rover is in position (0,0) facing north") {
-      val rover = Rover(0,0, North)
-      rover shouldEqual Rover(0,0, North)
+    it("rover is in position (0,0) facing north") {
+      val rover = Rover(0, 0, North)
+      rover shouldEqual Rover(0, 0, North)
     }
 
-    it(s"rover moves forward facing north") {
-      val rover = Rover(0,0, North)
-      rover.move(Array("f")) shouldEqual Rover(0,1, North)
+    it("rover moves forward facing north") {
+      val rover = Rover(0, 0, North)
+      rover.move(Array("f")) shouldEqual Rover(0, 1, North)
     }
 
-    it(s"rover moves backward facing north") {
-      val rover = Rover(0,0, North)
-      rover.move(Array("b")) shouldEqual Rover(0,-1, North)
+    it("rover moves backward facing north") {
+      val rover = Rover(0, 0, North)
+      rover.move(Array("b")) shouldEqual Rover(0, -1, North)
     }
+
+    it("rover moves forward facing north starting in (1,1)") {
+      val rover = Rover(1, 1, North)
+      rover.move(Array("f")) shouldEqual Rover(1, 2, North)
+    }
+
+
   }
 }
 
