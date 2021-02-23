564593f1dd17b64f182f011ec035077b0f56f7c5
Javier Lopez <javisan81@hotmail.com>
2021-02-23 08:58:34 +0100

    can create the rover

--- src/test/scala/Rover.scala
+++ src/test/scala/Rover.scala
@@ -1,11 +1,15 @@
-
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 
-class Rover extends AnyFunSpec {
+abstract case class Direction()
+object North extends Direction
+case class Rover(x: Int, y: Int, direction: Direction)
+
+class RoverTest extends AnyFunSpec {
   describe("Rover") {
-    it(s"should be centered in 0,0 facing each direction") {
-      true shouldEqual true;
+    it(s"rover is in position (0,0) facing north") {
+      val rover = Rover(0,0, North)
+      rover shouldEqual Rover(0,0, North)
     }
   }
 }
